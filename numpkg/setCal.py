6ff69390d9cbdd4f08292c593d838623a17560f4
jwc406 <77850103+jwc406@users.noreply.github.com>
2022-04-27 16:41:20 +0900

    Update setCal.py

--- numpkg/setCal.py
+++ numpkg/setCal.py
@@ -7,5 +7,5 @@ set_2nd = set(list_2nd)
 union = set_1st.union(set_2nd)
 intersection = set_1st.intersection(set_2nd)
 
-print("union: ", list(union))
-print("intersection: ", list(intersection))
+print("union:", list(union))
+print("intersection:", list(intersection))
